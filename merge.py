df4a711eb42744167b2025ddc8bd01f3eb9a682a
Donglin Wang <wdl448@yahoo.com>
2022-02-15 20:55:38 -0500

    CODE: Added ParNet and ShapeNet visualization

--- merge.py
+++ merge.py
@@ -1,27 +1,40 @@
 import os
-
+import collections
 
 ACRONYM_PATH = os.path.join("/", "home", "donglin", "Data", "acronym", "grasps")
 cnt = 0
 acronym_file_list = os.listdir(ACRONYM_PATH)
 acronym_ids = []
+acronym_id_to_scale = collections.defaultdict(float)
 for filename in acronym_file_list:
-    id = filename.split("_")[1]
+    tokens = filename.split("_")
+    id = tokens[1]
     if len(filename) >= 28:
         acronym_ids.append(id)
-
-print(len(acronym_ids))
-
+        acronym_id_to_scale[id] = float(tokens[-1][:-3])
 
 PARTNET_PATH = os.path.join('/', 'home', 'donglin', 'Github', 'partnet_dataset', 'stats', 'all_valid_anno_info.txt')
-partnet_ids = set()
+partnet_id_to_cat = collections.defaultdict(str)
+categories_to_ids = collections.defaultdict(list)
+id_to_anno = collections.defaultdict(str)
+anno_to_id = collections.defaultdict(str)
+category_count = collections.Counter()
 
 with open(PARTNET_PATH, 'r') as file_obj:
     for line in file_obj.readlines():
-        partnet_ids.add(line.split(" ")[3])
+        tokens = line.split(" ")
+        partnet_id_to_cat[tokens[3]] = tokens[2]
+        anno_to_id[tokens[0]] = tokens[3]
+        id_to_anno[tokens[3]] = tokens[0]
 
 hits = 0
 for id in acronym_ids:
-    if id in partnet_ids:
+    if id in partnet_id_to_cat:
         hits += 1
-print(hits)
+        categories_to_ids[partnet_id_to_cat[id]].append(id)
+        category_count[partnet_id_to_cat[id]] += 1
+        
+print(category_count)
+print(categories_to_ids["Mug"][1])
+print(id_to_anno[categories_to_ids["Mug"][1]])
+print(acronym_id_to_scale[categories_to_ids["Mug"][1]])
---
+++ visualize_mesh_pointcloud.py
@@ -0,0 +1,44 @@
+import os
+
+import numpy as np
+import pybullet as p
+
+MESH_ID = "7223820f07fd6b55e453535335057818"
+POINT_ID = "8745"
+### For the ACRONYM Dataset, irrelevent for to the issue
+# MESH_SCALE = 0.015551998509716471
+# MESH_PATH = f'/home/donglin/Data/acronym/meshes/Mug/{MESH_ID}.obj'
+POINT_PATH = f'/home/donglin/Data/data_v0/{POINT_ID}/point_sample/sample-points-all-pts-label-10000.ply'
+SHAPNET_MESH_PATH = '/home/donglin/Data/ShapeNetCore.v2/03797390/7223820f07fd6b55e453535335057818/models/model_normalized.obj'
+
+p.connect(p.GUI)
+def load_visual_obj(path, position=[0, 0, 0], orientation=[0, 0, 0, 1]):
+    shape_id = p.createVisualShape(shapeType=p.GEOM_MESH,
+                                   fileName=path,
+                                   flags=p.GEOM_FORCE_CONCAVE_TRIMESH |
+                                   p.GEOM_CONCAVE_INTERNAL_EDGE,
+                                #    meshScale=[MESH_SCALE]*3
+                                   )
+    return p.createMultiBody(baseVisualShapeIndex=shape_id, basePosition=position, baseOrientation=orientation)
+
+
+def load_point(position):
+    shape_id = p.createVisualShape(shapeType=p.GEOM_SPHERE,
+                                   radius=0.03,
+                                   rgbaColor=[0,0,1,1]
+                                   )
+    return p.createMultiBody(baseVisualShapeIndex=shape_id, basePosition=position)
+
+
+pts = []
+with open(POINT_PATH, 'r') as pts_f:
+    tokens = pts_f.readlines()
+    for token in tokens[10:]:
+        pts.append([float(ele) for ele in token.split()[:3]])
+pts = np.array(pts)
+# pts[:, [1,2]] = pts[:, [2,1]]
+load_visual_obj(SHAPNET_MESH_PATH)
+for pt in pts:
+    load_point(pt)
+while 1:
+    p.stepSimulation()
